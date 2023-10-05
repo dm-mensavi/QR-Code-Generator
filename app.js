0237cddb3d7f27983cc61c7409937075e210bbcf
dm-mensavi <mensavidavid@gmail.com>
2023-10-05 15:59:00 +0000

    Add donwload functionality

--- app.js
+++ app.js
@@ -8,40 +8,32 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-
-
 const app = express();
 const port = 5000;
 var imgSrc = "defaultQR.png";
+var inputUrl = "";
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
 app.get("/", (req, res) => {
-  res.render('index', {imgSrc});
-});  
-
+	res.render("index", { imgSrc, inputUrl });
+});
 
 app.post("/", (req, res) => {
+	var source = "'" + req.body.url + "'";
 
-  var source = "'" + req.body.url + "'";
+	var qr_png = qr.image(source, { type: "png" });
+	qr_png.pipe(fs.createWriteStream("./public/QR.png"));
 
-  console.log(source);
-  
-  var qr_png = qr.image(source, {type: 'png'});
-  qr_png.pipe(fs.createWriteStream('./public/QR.png'));
+	imgSrc = "QR.png";
 
-  imgSrc = 'QR.png';
+	res.render("index", { imgSrc, inputUrl });
+});
 
-  res.render('index', {imgSrc});
-
-})
-
-  app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-  });
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
